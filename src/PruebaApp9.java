e19eba4e18a5a778ac0651177876bf81a74a25c4
josepmartorell <martorelljosep@gmail.com>
2022-07-08 17:43:36 +0200

    ejercicio9

---
+++ src/PruebaApp9.java
@@ -0,0 +1,15 @@
+
+public class PruebaApp9 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for(int i = 1; i <= 100; i++) {
+			if(((i % 2) == 0) && ((i % 3) == 0))
+			System.out.println(i+" es divisible por 2 y por 3");
+
+		}
+
+	}
+
+}
