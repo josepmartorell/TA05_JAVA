619ad34630338ada9b40edcba70944e164fdbe7c
josepmartorell <martorelljosep@gmail.com>
2022-07-08 17:12:54 +0200

    ejercicio3

---
+++ src/PruebaApp3.java
@@ -0,0 +1,15 @@
+import javax.swing.JOptionPane;
+
+public class PruebaApp3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub	
+		
+		String nombre = JOptionPane.showInputDialog("Introduzca su nombre");
+		JOptionPane.showMessageDialog(null, "Bienvenido "+nombre);
+
+		
+		
+	}
+
+}
