ba1eaab5fb23d826ba0aaf94acb6f657c4d7d17a
josepmartorell <martorelljosep@gmail.com>
2022-07-08 17:41:14 +0200

    ejercicio7

---
+++ src/PruebaApp7.java
@@ -0,0 +1,14 @@
+
+public class PruebaApp7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int count = 1;
+		while(count <= 100) {
+			System.out.println("Count = "+count);
+			count++;
+		}
+	}
+
+}
